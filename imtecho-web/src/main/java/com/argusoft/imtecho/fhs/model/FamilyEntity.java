90932c0ede2027abffcf4428e7e317c1d056b421
Rushi Jobanputra <rjobanputra@argusoft.com>
2025-01-20 16:00:50 +0530

    removed vip type column

--- imtecho-web/src/main/java/com/argusoft/imtecho/fhs/model/FamilyEntity.java
+++ imtecho-web/src/main/java/com/argusoft/imtecho/fhs/model/FamilyEntity.java
@@ -219,8 +219,8 @@ public class FamilyEntity extends EntityAuditInfo implements Serializable {
     @Column(name = "is_iec_given")
     private Boolean isIecGiven;
 
-    @Column(name = "vip_type")
-    private String vipType;
+//    @Column(name = "vip_type")
+//    private String vipType;
 
     public void setWasteDisposalDetails(Set<Integer> wasteDisposalDetails) {
         this.wasteDisposalDetails = wasteDisposalDetails;
