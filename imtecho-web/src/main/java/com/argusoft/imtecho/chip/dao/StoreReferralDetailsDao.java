bd674f23d110e27dd7c99dcca4065b6230a01c2e
Himanshu <hjaiswal@argusoft.com>
2025-05-13 18:12:58 +0530

    added flyway change and dao

---
+++ imtecho-web/src/main/java/com/argusoft/imtecho/chip/dao/StoreReferralDetailsDao.java
@@ -0,0 +1,7 @@
+package com.argusoft.imtecho.chip.dao;
+
+import com.argusoft.imtecho.chip.model.StoreReferralDetails;
+import com.argusoft.imtecho.database.common.GenericDao;
+
+public interface StoreReferralDetailsDao extends GenericDao<StoreReferralDetails, Integer> {
+}
